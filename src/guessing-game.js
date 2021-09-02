6ab8b8fbd8548c09a24729385bfbe9a218bf67b4
igo-l <igolllf@gmail.com>
2021-09-02 23:13:04 +0300

    feat: solve the task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    this.min;
+    this.max;
+    this.candidate;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.candidate = Math.round((this.max + this.min)/2);
     }
 
     lower() {
-
+        this.max = this.candidate;
     }
 
     greater() {
-
+        this.min = this.candidate;
     }
 }
 
